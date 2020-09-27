b47f58c38b8d063ff25debe218391d6230332394
vivekptnk <vivekpattanaik100@gmail.com>
2020-09-27 04:36:08 -0700

    initial commit

---
+++ mish.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+
+
+void command_loop()
+{	
+	int exit_counter = 1;
+	
+	do{
+	printf("MISH >\n");
+	
+	}while(exit_counter = 1);
+}
+
+
+int  cd(){
+	return 0;
+}
+
+int pwd(){
+	return 0;
+}
+
+int  exit_cmd(){
+	return 0;
+}
+
+int  log_file(){
+	return 0;
+}
+
+int  fork_exec(){
+	return 0;
+}
+
+int  main(){
+        printf("Starting Up !\n");
+        command_loop();
+        return 0;
+}
